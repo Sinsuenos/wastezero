e0d97575c9e2affe7f5defe41df5733bdf0b552f
Chris <sinaloainspireddreams@gmail.com>
2026-08-27 20:23:54 -0700

    Remove unused build.js

--- build.js
+++
@@ -1,5 +0,0 @@
-// Vercel build script - runs cd client && npm install && npm run build
-const { execSync } = require('child_process');
-console.log('Building client...');
-execSync('cd client && npm install && npm run build', { stdio: 'inherit' });
-console.log('Build complete!');
