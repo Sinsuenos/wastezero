9a11585b5c3130fc4a825e5b82be40c87d43c53b
Chris <sinaloainspireddreams@gmail.com>
2026-08-27 23:29:22 -0700

    Add debug log to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, useNavigate, useLocation } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import App from './App';
 import './index.css';
+
+console.log('WasteZero starting...');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
